85167cb0d5101768d01eed30e78526fe18c2355a
ALEXANDER_MOSIN <sasha.mosin89@gmail.com>
2021-02-21 22:29:09 +0300

    add project LettersCount

---
+++ practice/LettersCount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LettersCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            string str = Console.ReadLine();
+            int num = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    num = num + 1;
+                }
+            }
+            Console.WriteLine("Количество букв равно {0}", num);
+            Console.ReadKey();
+        }
+    }
+}
