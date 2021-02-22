d227a4298dbc234e14d4a07e5e701dbfa6e8ab90
ALEXANDER_MOSIN <sasha.mosin89@gmail.com>
2021-02-23 00:19:10 +0300

    added third method and start last task

--- Practice-7/Practice-7/Program.cs
+++ Practice-7/Practice-7/Program.cs
@@ -8,6 +8,8 @@ namespace Practice_7
         {
             MakeEmptyArray();
             MaxValinArray();
+            Array3el();
+            //BoxUnbox();
         }
         static void MakeEmptyArray()
         {
@@ -18,11 +20,14 @@ namespace Practice_7
         {
             Random rand = new Random();
             int[] array2 = new int[13];
-            int max = 0;
             for (int i = 0; i < array2.Length; i++)
             {
-                array2[i] = rand.Next(0,10);
+                array2[i] = rand.Next(0, 10);
                 Console.Write(array2[i] + " ");
+            }
+            int max = array2[0];
+            for (int i = 1; i < array2.Length; i++)
+            {
                 if (array2[i] > max)
                     max = array2[i];
             }
@@ -33,8 +38,28 @@ namespace Practice_7
         {
             string[] array3 = new string[3];
             array3[0] = "32";
-
-            
+            array3[1] = "A";
+            array3[2] = "Hello";
+            for (int i = 0; i < array3.Length; i++)
+            {
+                Console.WriteLine(array3[i]);
+            }
+            array3[0] = Convert.ToString(Convert.ToInt32(array3[0]) + 10);
+            array3[2] = "Hello" + ", guys";
+            for (int i = 0; i < array3.Length; i++)
+            {
+                Console.WriteLine(array3[i]);
+            }
         }
+        //static void BoxUnbox()
+        //{
+        //    Console.WriteLine("Введите число");
+        //    short a = Convert.ToInt16(Console.ReadLine());
+        //    object b = a;
+        //    sbyte c = (sbyte)b;
+        //    Console.WriteLine(b);
+
+        //}
+        
     }
 }
