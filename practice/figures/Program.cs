507c4fbcae377ae28d7607fb467c0d44132ee243
ALEXANDER_MOSIN <sasha.mosin89@gmail.com>
2021-03-17 19:49:52 +0300

    just start to do practice

---
+++ practice/figures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"This is {}. CLR Type is {}. Square is {}.");
+        }
+    }
+}
+interface IFigures
+{
+    void Square()
+    {
+
+    }
+}
+class Circle : IFigures
+{
+    
+    public Circle(int radius)
+    {
+        radius =Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+class Kvadrat : IFigures
+{ 
+
+}
+class Triangle : IFigures
+{
+
+}
